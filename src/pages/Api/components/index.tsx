8bb49d5309673bf9234608cbe6dfeaecb29dbbbe
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-08-07 09:50:50 +0900

    refactor: loop切り分けた

--- src/pages/Api/components/index.tsx
+++ src/pages/Api/components/index.tsx
@@ -1,29 +1,21 @@
 import React, { FC } from 'react';
-import ApiQuery from '@api/graphql/queries/pages/Api.graphql';
+import gqlApi from '@api/graphql/queries/pages/Api.graphql';
 import { useQuery } from '@apollo/react-hooks';
 import { ApiQuery_organization_repositories_nodes } from '@api/graphql/queries/pages/__generated__/ApiQuery';
 
 const Api: FC = () => {
-  const { loading, data } = useQuery(ApiQuery);
+  const { loading, data } = useQuery(gqlApi);
 
-  return (
-    <>
-      {loading ? (
-        <p>Loading ......</p>
-      ) : (
-        <ul>
-          {data.organization.repositories.nodes.map(
-            (node: ApiQuery_organization_repositories_nodes, index: number) => (
-              <li key={node.id}>
-                <h1>{index + 1}</h1>
-                <a href={node.url}>{node.name}</a>
-              </li>
-            )
-          )}
-        </ul>
-      )}
-    </>
+  const list = data.organization.repositories.nodes.map(
+    (node: ApiQuery_organization_repositories_nodes, index: number) => (
+      <li key={node.id}>
+        <h1>{index + 1}</h1>
+        <a href={node.url}>{node.name}</a>
+      </li>
+    )
   );
+
+  return <>{loading ? <p>Loading ......</p> : <ul>{list}</ul>}</>;
 };
 
 export default Api;
