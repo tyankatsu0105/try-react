27a2b34f6474b199f0a3d2ae5acb7ccc391eeb9e
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-08-21 13:48:57 +0900

    refactor: package整理、helmet使用してみた

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,14 @@
 import React, { FunctionComponent } from 'react';
+import { Helmet } from 'react-helmet';
 
 import { AboutTab } from './components/AboutTab';
 
 const About: FunctionComponent = () => (
   <>
+    <Helmet>
+      <title>This is About</title>
+      <meta name="description" content="About desc" />
+    </Helmet>
     <AboutTab />
   </>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
 import React, { FunctionComponent } from 'react';
+import { Helmet } from 'react-helmet';
 
 const Home: FunctionComponent = () => {
   return (
     <>
+      <Helmet>
+        <title>This is Home</title>
+        <meta name="description" content="Home desc" />
+      </Helmet>
       <p className="Home">Hi!!!</p>
       <p className="Home Home-2">This is</p>
       <p id="Home">Home</p>
