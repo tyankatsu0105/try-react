5deb716a37f00a0c4da4ba471b8c71fc26da930c
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-08-26 17:43:45 +0900

    feat: material uiとformik組み合わせた

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,76 @@
 import React, { FunctionComponent } from 'react';
+import { useField, Formik, FormikProps } from 'formik';
+import * as Yup from 'yup';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+} from '@material-ui/core';
 
-const Home: FunctionComponent = () => {
-  return <div>aaa</div>;
+interface Props {
+  name: string;
+  label: string;
+  type: string;
+}
+
+const MyTextField: FunctionComponent<Props> = (props) => {
+  const { label, ...restProps } = props;
+  const [field, meta] = useField(restProps);
+  const { touched, error } = meta;
+
+  const handleError = touched && error ? true : false;
+
+  return (
+    <>
+      <FormControl error={handleError}>
+        <InputLabel htmlFor="my-input">{label}</InputLabel>
+        <Input {...field} id="my-input" aria-describedby="my-helper-text" />
+
+        <FormHelperText id="my-helper-text">
+          {touched && error ? error : '入力してください'}
+        </FormHelperText>
+      </FormControl>
+    </>
+  );
 };
+
+const intialValues = { user: '', email: '' };
+type Values = typeof intialValues;
+
+const validationSchema = Yup.object().shape({
+  user: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .min(10, 'Too Short!')
+    .max(50, 'Too Long!')
+    .email('lllllll')
+    .required('Required'),
+});
+
+const Home = () => (
+  <div>
+    <h1>My Form</h1>
+    <Formik
+      initialValues={intialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+      render={(props: FormikProps<Values>) => (
+        <form onSubmit={props.handleSubmit} autoComplete="off">
+          <MyTextField name="user" type="text" label="ユーザー名" />
+          <MyTextField name="email" type="text" label="メールアドレス" />
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    />
+  </div>
+);
 
 export default Home;
