f59100af52b4e25be2ba7ce9c78925d99ef18ccf
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-08-26 18:22:56 +0900

    refactor: useFormik使った

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { useField, Formik, FormikProps } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import {
   FormControl,
@@ -8,69 +8,52 @@ import {
   FormHelperText,
 } from '@material-ui/core';
 
-interface Props {
-  name: string;
-  label: string;
-  type: string;
+interface FormValues {
+  user: string;
 }
 
-const MyTextField: FunctionComponent<Props> = (props) => {
-  const { label, ...restProps } = props;
-  const [field, meta] = useField(restProps);
-  const { touched, error } = meta;
-
-  const handleError = touched && error ? true : false;
-
+const Home: FunctionComponent = () => {
+  const { values, errors, touched, handleChange, handleSubmit } = useFormik<
+    FormValues
+  >({
+    initialValues: {
+      user: '',
+    },
+    validationSchema: Yup.object().shape({
+      user: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('入力必須'),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <>
-      <FormControl error={handleError}>
-        <InputLabel htmlFor="my-input">{label}</InputLabel>
-        <Input {...field} id="my-input" aria-describedby="my-helper-text" />
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <FormControl error={touched.user && errors.user ? true : false}>
+          <InputLabel htmlFor="my-input">ユーザー名</InputLabel>
+          <Input
+            type="input"
+            name="user"
+            value={values.user}
+            id="my-input"
+            aria-describedby="my-helper-text"
+            onChange={handleChange}
+          />
 
-        <FormHelperText id="my-helper-text">
-          {touched && error ? error : '入力してください'}
-        </FormHelperText>
-      </FormControl>
+          <FormHelperText id="my-helper-text">
+            {touched.user && errors.user
+              ? errors.user
+              : 'ユーザー名を入力してください'}
+          </FormHelperText>
+        </FormControl>
+
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 };
-
-const intialValues = { user: '', email: '' };
-type Values = typeof intialValues;
-
-const validationSchema = Yup.object().shape({
-  user: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string()
-    .min(10, 'Too Short!')
-    .max(50, 'Too Long!')
-    .email('lllllll')
-    .required('Required'),
-});
-
-const Home = () => (
-  <div>
-    <h1>My Form</h1>
-    <Formik
-      initialValues={intialValues}
-      validationSchema={validationSchema}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-      render={(props: FormikProps<Values>) => (
-        <form onSubmit={props.handleSubmit} autoComplete="off">
-          <MyTextField name="user" type="text" label="ユーザー名" />
-          <MyTextField name="email" type="text" label="メールアドレス" />
-          <button type="submit">Submit</button>
-        </form>
-      )}
-    />
-  </div>
-);
 
 export default Home;
