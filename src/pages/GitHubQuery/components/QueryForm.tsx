65d5bcbbd06a81e27ea1bc8e009f6220b875fcad
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2019-08-29 14:55:25 +0900

    refactor: containerでuseQuery、presentationでuseFormikした
    型が雑に置かれていて気持ち悪い

--- src/pages/GitHubQuery/components/QueryForm.tsx
+++ src/pages/GitHubQuery/components/QueryForm.tsx
@@ -1,9 +1,137 @@
 import React, { FunctionComponent } from 'react';
 
-export const QueryForm: FunctionComponent = () => {
+import {
+  GetOrganization,
+  GetOrganizationVariables,
+} from '../containers/api/__generated__/GetOrganization';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  FormGroup,
+  Button,
+  Box,
+  Typography,
+  CircularProgress,
+  Divider,
+  Link,
+} from '@material-ui/core';
+
+type FormValues = GetOrganizationVariables;
+
+interface Props {
+  loading: boolean;
+  data: GetOrganization | undefined;
+  refetch: (values: object) => void;
+}
+
+export const QueryForm: FunctionComponent<Props> = (props) => {
+  const { loading, data, refetch } = props;
+
+  const { values, errors, touched, handleChange, handleSubmit } = useFormik<
+    FormValues
+  >({
+    initialValues: {
+      login: '',
+      first: 1,
+    },
+    validationSchema: Yup.object().shape({
+      login: Yup.string().required('入力必須'),
+      first: Yup.number()
+        .min(1)
+        .required('入力必須'),
+    }),
+    onSubmit: (values) => {
+      refetch(values);
+    },
+  });
   return (
     <>
-      <a href="">aaaaa</a>
+      {/* <QueryFormComponent /> */}
+      <form action="" onSubmit={handleSubmit} autoComplete="off">
+        <FormGroup>
+          <Box marginTop={2}>
+            <FormControl error={touched.login && errors.login ? true : false}>
+              <InputLabel htmlFor="login">organization</InputLabel>
+              <Input
+                type="input"
+                name="login"
+                value={values.login}
+                id="login"
+                aria-describedby="input-login"
+                onChange={handleChange}
+              />
+
+              <FormHelperText id="login">
+                {touched.login && errors.login
+                  ? errors.login
+                  : 'organization名を入力'}
+              </FormHelperText>
+            </FormControl>
+          </Box>
+
+          <Box marginTop={2}>
+            <FormControl error={touched.first && errors.first ? true : false}>
+              <InputLabel htmlFor="first">organization</InputLabel>
+              <Input
+                type="number"
+                name="first"
+                value={values.first}
+                id="first"
+                aria-describedby="input-first"
+                onChange={handleChange}
+              />
+
+              <FormHelperText id="first">
+                {touched.first && errors.first
+                  ? errors.first
+                  : '表示するレポジトリ数を入力'}
+              </FormHelperText>
+            </FormControl>
+          </Box>
+
+          <Box marginTop={2} display="flex" justifyContent="center">
+            <Button type="submit" variant="contained" color="primary">
+              submit
+            </Button>
+          </Box>
+        </FormGroup>
+      </form>
+
+      {loading && (
+        <>
+          <CircularProgress></CircularProgress>
+        </>
+      )}
+
+      {data &&
+        data.organization &&
+        data.organization.repositories &&
+        data.organization.repositories.nodes && (
+          <>
+            {data.organization.repositories.nodes.map((node) => {
+              return (
+                node && (
+                  <>
+                    <Box marginTop={1}>
+                      <Link href={node.url}>
+                        <Typography variant="h2">{node.name}</Typography>
+                      </Link>
+                      <Typography paragraph={true}>
+                        {node.description}
+                      </Typography>
+                    </Box>
+                    <Divider />
+                  </>
+                )
+              );
+            })}
+          </>
+        )}
     </>
   );
 };
--- src/pages/GitHubQuery/containers/QueryForm.tsx
+++ src/pages/GitHubQuery/containers/QueryForm.tsx
@@ -7,28 +7,7 @@ import {
   GetOrganizationVariables,
 } from './api/__generated__/GetOrganization';
 
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import {
-  FormControl,
-  InputLabel,
-  Input,
-  FormHelperText,
-  FormGroup,
-  Button,
-  Box,
-  Typography,
-  CircularProgress,
-  Divider,
-  Link,
-} from '@material-ui/core';
-
-interface FormValues {
-  login: string;
-  first: number;
-}
-
-// import { QueryFormComponent } from '../components';
+import { QueryFormComponent } from '../components';
 
 export const QueryForm: FunctionComponent = () => {
   const variables: GetOrganizationVariables = {
@@ -42,106 +21,15 @@ export const QueryForm: FunctionComponent = () => {
     }
   );
 
-  const { values, errors, touched, handleChange, handleSubmit } = useFormik<
-    FormValues
-  >({
-    initialValues: {
-      login: '',
-      first: 1,
-    },
-    validationSchema: Yup.object().shape({
-      login: Yup.string().required('入力必須'),
-      first: Yup.number()
-        .min(1)
-        .required('入力必須'),
-    }),
-    onSubmit: (values) => {
-      refetch(values);
-    },
-  });
+  const props = {
+    loading,
+    data,
+    refetch,
+  };
+
   return (
     <>
-      {/* <QueryFormComponent /> */}
-      <form action="" onSubmit={handleSubmit} autoComplete="off">
-        <FormGroup>
-          <Box marginTop={2}>
-            <FormControl error={touched.login && errors.login ? true : false}>
-              <InputLabel htmlFor="login">organization</InputLabel>
-              <Input
-                type="input"
-                name="login"
-                value={values.login}
-                id="login"
-                aria-describedby="input-login"
-                onChange={handleChange}
-              />
-
-              <FormHelperText id="login">
-                {touched.login && errors.login
-                  ? errors.login
-                  : 'organization名を入力'}
-              </FormHelperText>
-            </FormControl>
-          </Box>
-
-          <Box marginTop={2}>
-            <FormControl error={touched.first && errors.first ? true : false}>
-              <InputLabel htmlFor="first">organization</InputLabel>
-              <Input
-                type="number"
-                name="first"
-                value={values.first}
-                id="first"
-                aria-describedby="input-first"
-                onChange={handleChange}
-              />
-
-              <FormHelperText id="first">
-                {touched.first && errors.first
-                  ? errors.first
-                  : '表示するレポジトリ数を入力'}
-              </FormHelperText>
-            </FormControl>
-          </Box>
-
-          <Box marginTop={2} display="flex" justifyContent="center">
-            <Button type="submit" variant="contained" color="primary">
-              submit
-            </Button>
-          </Box>
-        </FormGroup>
-      </form>
-
-      {loading && (
-        <>
-          <CircularProgress></CircularProgress>
-        </>
-      )}
-
-      {data &&
-        data.organization &&
-        data.organization.repositories &&
-        data.organization.repositories.nodes && (
-          <>
-            {data.organization.repositories.nodes.map((node) => {
-              return (
-                node && (
-                  <>
-                    <Box marginTop={1}>
-                      <Link href={node.url}>
-                        <Typography variant="h2">{node.name}</Typography>
-                      </Link>
-                      <Typography paragraph={true}>
-                        {node.description}
-                      </Typography>
-                    </Box>
-                    <Divider />
-                  </>
-                )
-              );
-            })}
-          </>
-        )}
+      <QueryFormComponent {...props} />
     </>
   );
 };
